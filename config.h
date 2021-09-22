136cd4296d6b2ec450d983d8cd755f17e0b24784
wantyapps <56798701+wantyapps@users.noreply.github.com>
2021-09-22 22:44:18 +0300

    Make config more like the default config

--- config.h
+++ config.h
@@ -1,4 +1,4 @@
 //: If you want to compile wSH with debug output (verbose), uncomment the following line
-// #define WSH_DEBUG_MODE 1
-//: And comment this line
 #define WSH_DEBUG_MODE 1
+//: And comment this line
+//#define WSH_DEBUG_MODE 0
